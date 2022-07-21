cd7a153e03c2a174c915955b76224c8ddca50854
laurismelderis <lauris.melderis77@gmail.com>
2022-07-21 17:37:36 +0300

    Add jest

---
+++ src/services/fakeMovieService.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { getMovies, getMovie } from './fakeMovieService'
+
+describe('Fake movie service', () => {
+    it('should return movies', () => {
+        expect(true).toBeTruthy()
+    })
+})
